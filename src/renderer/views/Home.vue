bca11707a02ef92baf7d336ae75b9983d697dbb7
fuyiwei <601556811@qq.com>
2020-09-04 17:18:19 +0800

    主界面逻辑关联修改

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -3,7 +3,7 @@
     <el-container>
       <el-container>
         <el-aside style="width:270px;border:2px solid silver;height:100%;">
-          <div style="height:35%;">
+          <div style="height:35%;overflow:auto;">
             <div style="height:auto;background-color: rgba(49, 110, 175, 0.95);">
              <span style="margin-left:10px;color: white;">图层控制</span>
             </div>
@@ -17,7 +17,7 @@
               highlight-current
               :props="defaultProps">
               <span class="custom-tree-node" slot-scope="{ node,data }">
-                <span style="margin-left:5px"> 
+                <span style="margin-left:5px" @click="handleLayerClick(node)"> 
                   <i :class="data.icon"></i>{{ node.label }}
                 </span>              
               </span>
@@ -25,7 +25,7 @@
           </div>
           <div style="height:65%;"> 
             <el-tabs type="border-card" style="height:100%;">
-              <el-tab-pane label="点编辑">
+              <el-tab-pane label="点编辑" style="display:none;" :class="{ ptShow : this.curLayerId == this.ptId ,ptHide : this.curLayerId != this.ptId}" >
                 <el-switch
                   v-model="addPoint"
                   active-text=""
@@ -40,7 +40,7 @@
                 </el-switch>
 
               </el-tab-pane>
-              <el-tab-pane label="线编辑">  
+              <el-tab-pane label="线编辑" style="display:none;">  
                 <el-switch
                   v-model="addLine"
                   active-text=""
@@ -54,20 +54,19 @@
                   style="padding-left:30px;">
                 </el-switch>
               </el-tab-pane>
-              <el-tab-pane label="网格编辑">
-              <el-form  class="demo-form-inline" style="padding-left:10px;">
-                <el-form-item label="设置等分数" >
-                  <el-input-number v-model="gridNum" controls-position="right" @change="handleGridChange" :min="0" :max="256" size="small"></el-input-number>
-                </el-form-item>
-                <el-form-item>
-                  <el-button-group>
-                    <el-button type="primary" >X2</el-button>
-                    <el-button type="primary" >X1/2</el-button>
-                    <el-button type="primary" icon="el-icon-delete">归零</el-button>
-                  </el-button-group>
-                </el-form-item>
-              </el-form>
-                
+              <el-tab-pane label="网格编辑" style="display:none;">
+                <el-form  class="demo-form-inline" style="padding-left:10px;">
+                  <el-form-item label="设置等分数" >
+                    <el-input-number v-model="gridNum" controls-position="right" @change="handleGridChange" :min="0" :max="256" size="small"></el-input-number>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button-group>
+                      <el-button type="primary" >X2</el-button>
+                      <el-button type="primary" >X1/2</el-button>
+                      <el-button type="primary" icon="el-icon-delete">归零</el-button>
+                    </el-button-group>
+                  </el-form-item>
+                </el-form>
               </el-tab-pane>
               <el-tab-pane label="画布工具">比如 撤销 重做 平移 缩放 旋转</el-tab-pane>
               <el-tab-pane label="选择工具">支持点选 框选 然后操作选中要素</el-tab-pane>
@@ -75,7 +74,7 @@
           </div>
         </el-aside>
          <div style="overflow: hidden;z-index: 9999;position: fixed;left:270px;top:30%
-         ;background-color:silver;height:40px;border-bottom-right-radius: 4px;border-top-right-radius: 4px;" >
+         ;background-color:silver;height:40px;border-bottom-right-radius: 4px;border-top-right-radius: 4px;" @click = "handleTreeVis">
           <i class="el-icon-arrow-left" style="padding-top:12px;"></i>
         </div>
         <el-main style="background-color:#f3f3f3">
@@ -92,7 +91,8 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'Home',
   data: function () {
-    
+    let curLayerId = -1;
+    let ptId = 3;
     const treeData =  [{
           id: 1,
           label: '画布图层',
@@ -104,7 +104,7 @@ export default Vue.extend({
               icon: 'node_grid',
             },
             {
-              id: 3,
+              id: ptId,
               label: '节点',
               icon: 'node_vertex',
             }, 
@@ -137,11 +137,25 @@ export default Vue.extend({
         delPoint:false,
         addLine:false,
         delLine:false,
+        curLayerId,
+        ptId,
       }
   },
   methods:{
      handleGridChange(value: any) {
         console.log(value);
+      },
+      handleTreeVis(){
+        console.log(111);
+      },
+      handleLayerClick(node: any){
+        console.log(node.label);
+        this.curLayerId = node.id;
+        if(node.id == 3){
+         
+        }
+
+
       }
   },
   mounted:function(){
@@ -175,11 +189,18 @@ export default Vue.extend({
   background: url('/static/images/blueline.ico') no-repeat 0 3px;
  }
 
-   .node_a {
+  .node_a {
   padding-left: 20px;
   background: url('/static/images/greyline.ico') no-repeat 0 3px;
  }
 
 
+  .ptShow{
+    display: inline;
+  }
+
+  .ptHide{
+    display: none;
+  }
 
 </style>
